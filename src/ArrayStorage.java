6827d3025c99e9aba7ca5c64d2eb76e48a78c0b4
live_undead <zkvgarun@gmail.com>
2018-08-29 14:30:44 +0300

    Изминения от 29.08.18

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,28 +1,33 @@
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.Comparator;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    static int count;
     Resume[] storage = new Resume[10000];
 
     void clear() {
         Arrays.fill(storage, null);
+        count = 0;
     }
 
     void save(Resume r) {
         for(int i = 0; i < storage.length; i++){
             if(storage[i] == null){
                 storage[i] = r;
+                count++;
                 break;
             }
         }
     }
 
     Resume get(String uuid) {
-        for (Resume r : this.getAll()){
-            if (r.uuid.equals(uuid)) return r;
+        for (Resume r : getAll()){
+            if (r.uuid.equals(uuid)){
+                return r;
+            }
         }
         return new Resume();
     }
@@ -31,6 +36,8 @@ public class ArrayStorage {
         for(int i = 0; i < storage.length; i++){
             if(storage[i].uuid.equals(uuid)){
                 storage[i] = null;
+                count--;
+                getAll();
                 return;
             }
         }
@@ -40,10 +47,25 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
+        Arrays.sort(storage, new Comparator<Resume>() {
+            @Override
+            public int compare(Resume r1, Resume r2) {
+                if(r1 == null && r2 == null){
+                    return 0;
+                }
+                if(r1 == null){
+                    return 1;
+                }
+                if(r2 == null){
+                    return -1;
+                }
+                return r1.uuid.compareTo(r2.uuid);
+            }
+        });
+        return Arrays.copyOfRange(storage, 0, count);
     }
 
     int size() {
-        return this.getAll().length;
+        return count;
     }
 }
