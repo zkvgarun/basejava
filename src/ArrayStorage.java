06605d356f9cedaa83d1b4d38414e00d0e855000
live_undead <zkvgarun@gmail.com>
2018-08-31 03:15:35 +0300

    hw1 31.08.2018

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -14,17 +14,12 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for(int i = 0; i < storage.length; i++){
-            if(storage[i] == null){
-                storage[i] = r;
-                count++;
-                break;
-            }
-        }
+        storage[count] = r;
+        count++;
     }
 
     Resume get(String uuid) {
-        for (Resume r : getAll()){
+        for (Resume r : Arrays.copyOfRange(storage, 0, count)){
             if (r.uuid.equals(uuid)){
                 return r;
             }
@@ -37,7 +32,21 @@ public class ArrayStorage {
             if(storage[i].uuid.equals(uuid)){
                 storage[i] = null;
                 count--;
-                getAll();
+                Arrays.sort(storage, new Comparator<Resume>() {
+                    @Override
+                    public int compare(Resume r1, Resume r2) {
+                        if(r1 == null && r2 == null){
+                            return 0;
+                        }
+                        if(r1 == null){
+                            return 1;
+                        }
+                        if(r2 == null){
+                            return -1;
+                        }
+                        return r1.uuid.compareTo(r2.uuid);
+                    }
+                });
                 return;
             }
         }
@@ -47,21 +56,6 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Arrays.sort(storage, new Comparator<Resume>() {
-            @Override
-            public int compare(Resume r1, Resume r2) {
-                if(r1 == null && r2 == null){
-                    return 0;
-                }
-                if(r1 == null){
-                    return 1;
-                }
-                if(r2 == null){
-                    return -1;
-                }
-                return r1.uuid.compareTo(r2.uuid);
-            }
-        });
         return Arrays.copyOfRange(storage, 0, count);
     }
 
